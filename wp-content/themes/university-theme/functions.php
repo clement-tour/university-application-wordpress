f047cd6f9391c031505428820b5a6ffa6c14c4d2
clement-tour <86768151+clement-tour@users.noreply.github.com>
2022-04-12 19:36:57 +0400

    Lesson 14 ongoing : Elementor is working well

--- wp-content/themes/university-theme/functions.php
+++ wp-content/themes/university-theme/functions.php
@@ -2,7 +2,9 @@
 
 function university_files()
 {
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/university-static-master/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/university-static-master/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
