740e9c088367739bee64b26664bd0662db398670
clement-tour <86768151+clement-tour@users.noreply.github.com>
2022-04-17 12:35:21 +0400

    Lesson 14 finish index.php fill with html and css js in function.php

--- experiment.php
+++ experiment.php
@@ -1,5 +1,38 @@
-<?php echo 2+2; 
+<?php echo 2 + 2;
 $myName = 'John' ?>
 
-<h1>This is a test <?php echo $myName?></h1>
-<h2>Subtitle <?php echo $myName?></h2>
+<h1>This is a test <?php echo $myName ?></h1>
+<h2>Subtitle <?php echo $myName ?></h2>
+<?php
+function first($name, $age)
+{
+    echo "<p>My name is $name and I am $age</p>";
+}
+
+
+?>
+
+<?php echo 2 + 2;
+$myName = 'John' ?>
+
+<p>This is a test <?php echo $myName ?></p>
+<p>Subtitle <?php echo $myName ?></p>
+
+<p> <?php echo first('Jane', 33); ?></p>
+<p><?php echo first('John', 22); ?></p>
+
+<h1> <?php bloginfo('name'); ?></h1>
+<p> <?php bloginfo('description'); ?></p>
+
+<?php
+while (have_posts()) {
+    the_post();
+?>
+    <h2> <a href="<?php the_permalink() ?>"><?php the_title() ?>
+        </a>
+    </h2>
+    <?php the_content() ?>
+
+    <hr>
+<?php }
+?>
