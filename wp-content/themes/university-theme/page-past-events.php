013dd82bda12300d292390c38e55c81f1f7e03f8
clement-tour <86768151+clement-tour@users.noreply.github.com>
2022-04-22 11:07:32 +0400

    Lesson 35 done : Display past-events on page-past-event.php and filter event by date in the same file

--- wp-content/themes/university-theme/page-past-events.php
+++ wp-content/themes/university-theme/page-past-events.php
@@ -8,15 +8,37 @@ get_header();
         <h1 class="page-banner__title">
             All Events</h1>
         <div class="page-banner__intro">
-            <p>See what's going on</p>
+            <p>A recap of our past events</p>
         </div>
     </div>
 </div>
 
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
-        the_post(); ?>
+
+
+
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(array(
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        // All the following lines of this variable are here to sort the events by events date and hide the past events
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+
+        )
+    ));
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
@@ -37,15 +59,11 @@ get_header();
 
     <?php }
 
-    echo paginate_links()
+    echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+    ))
     ?>
 
-    <hr class="section-break">
-    <p>Looking for a recap of our past event ?
-        <a href="<?php echo site_url('/past-events') ?>">
-            Checkout our past events archive
-        </a>
-    </p>
 </div>
 
 <?php
